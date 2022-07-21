7c2d670817f4714a563fa00af7350618ec1690ae
AnanjayGurjar <ananjaygurjar@outlook.com>
2022-07-21 11:52:47 +0530

    added text

--- app/src/main/java/com/ananjay/composebasics/ui/components/CustomComponent.kt
+++ app/src/main/java/com/ananjay/composebasics/ui/components/CustomComponent.kt
@@ -3,11 +3,14 @@ package com.ananjay.composebasics.ui.components
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.ContentAlpha
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
@@ -17,19 +20,29 @@ import androidx.compose.ui.graphics.DefaultAlpha
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun CustomComponent(
     canvasSize: Dp = 300.dp,
     indicatorValue: Int = 0,
-    maxIndicatorValue: Int = 100,
+    maxIndicatorValue: Int = 1000,
     backgroundIndicatorColor: Color = MaterialTheme.colors.onSurface.copy(alpha = 0.1f),
     backgroundIndicatorStrokeWidth: Float = 100f,
     foregroundIndicatorColor: Color = MaterialTheme.colors.primary,
-    foregroundIndicatorStrokeWidth: Float = 100f
+    foregroundIndicatorStrokeWidth: Float = 100f,
+    bigText: Int,
+    bigTextFontSize: TextUnit = MaterialTheme.typography.h3.fontSize,
+    bigTextColor: Color = MaterialTheme.colors.onSurface,
+    bigTextSuffix: String = "GB",
+    smallText: String = "Remaining",
+    smallTextColor: Color = MaterialTheme.colors.onSurface.copy(alpha = 0.3f),
+    smallTextFontSize: TextUnit = MaterialTheme.typography.h6.fontSize
 ) {
     var allowedIndicatorValue by remember {
         mutableStateOf(maxIndicatorValue)
@@ -41,14 +54,14 @@ fun CustomComponent(
         maxIndicatorValue
     }
 
-    val animatedIndicatedValue = remember{
-        Animatable(initialValue = 0f)
+    var animatedIndicatedValue by remember{
+        mutableStateOf(0f)
     }
     LaunchedEffect(key1 = allowedIndicatorValue ){
-        animatedIndicatedValue.animateTo(indicatorValue.toFloat())
+        animatedIndicatedValue = allowedIndicatorValue.toFloat()
     }
 
-    val percentage = (animatedIndicatedValue.value/maxIndicatorValue) * 100
+    val percentage = (animatedIndicatedValue/maxIndicatorValue) * 100
 
     val sweepAngle by animateFloatAsState(
         //2.4, since max angle is 240
@@ -71,9 +84,19 @@ fun CustomComponent(
                 indicatorColor = foregroundIndicatorColor,
                 indicatorStrokeWidth = foregroundIndicatorStrokeWidth
             )
-        }
+        },
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        
+        EmbeddedElements(
+            bigText = allowedIndicatorValue,
+            bigTextFontSize = bigTextFontSize,
+            bigTextColor = bigTextColor,
+            bigTextSuffix = bigTextSuffix,
+            smallText = smallText,
+            smallTextColor = smallTextColor,
+            smallTextFontSize = smallTextFontSize
+        )
     }
 }
 fun DrawScope.backgroundIndicator(
@@ -119,6 +142,33 @@ fun DrawScope.foregroundIndicator(
             y = (size.height - componentSize.height)/2f
         )
     )
+}
+
+@Composable
+fun EmbeddedElements(
+    bigText: Int,
+    bigTextFontSize: TextUnit,
+    bigTextColor: Color,
+    bigTextSuffix: String,
+    smallText: String,
+    smallTextColor: Color,
+    smallTextFontSize: TextUnit
+){
+    Text(
+        text = smallText,
+        color= smallTextColor,
+        fontSize = smallTextFontSize,
+        textAlign = TextAlign.Center
+    )
+
+    Text(
+        text = "$bigText ${bigTextSuffix.take(2)}",
+        color= bigTextColor,
+        fontSize = bigTextFontSize,
+        textAlign = TextAlign.Center,
+        fontWeight = FontWeight.Bold
+    )
+
 }
 
 @Composable
